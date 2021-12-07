07a288e569ff244681f9a938fb832c1374ccd924
cristovantamayo <cristovan.tamayo@gmail.com>
2021-12-06 22:26:04 -0300

    folders structure

---
+++ src/main/java/dev/cristovantamayo/jwt/controller/HomeController.java
@@ -0,0 +1,13 @@
+package dev.cristovantamayo.jwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String home(){
+        return "Welcome to dev.cristovantamayo ;)";
+    }
+}
